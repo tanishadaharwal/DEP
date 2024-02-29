e545f216bfff04e7b812930bee66fd55edb095e0
SiddhiApraj7 <2021mcb1247@iitrpr.ac.in>
2024-02-29 23:02:47 +0530

    Landing Page

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -5,10 +5,15 @@ import React from 'react'
 export default function LandingPage() {
   return (
     <SafeAreaView className="bg-white h-full">
-        <View className="items-center mt-20 p-5 bg-white">
-            <Text class="text-xl text-purple-600">LandingPage</Text>
+      <View className="items-center mt-20 p-5 bg-white">
+        <Image className="h-40 w-40 mt-20 mb-4" source={require("../../assets/booksLogo.png")}></Image>
+        <Text className="font-light tracking-widest text-l pt-3 px-3 text-grey-30">WELCOME TO</Text>
+        <Text className="font-semibold text-xl p-3 text-[#0d64e5]">Streamlined Library Solutions</Text>
+        <View className="border-t-2 p-3 border-neutral-100">
+          <Text className=" font-extralight text-xs">An all-in-one solution to simplify the library experience</Text>
         </View>
-      
+      </View>
+
     </SafeAreaView>
   )
 }
