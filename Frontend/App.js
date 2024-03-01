24a48f63d105f84939ebfde3158893ceb4e6220c
Sakshi-196 <2021mcb1244@iitrpr.ac.in>
2024-03-02 01:54:15 +0530

    reading room added

--- Frontend/App.js
+++ Frontend/App.js
@@ -7,6 +7,10 @@ import "@expo/metro-runtime";
 import LandingPage from "./src/screens/LandingPage";
 import Register from './src/screens/Register';
 import Login from './src/screens/Login';
+import ReadingRoom from './src/screens/ReadingRoom';
+import LibraryPage from './src/screens/LibraryPage';
+import HomePage from './src/screens/HomePage';
+
 //Importing all screens
 export default function App() {
 
@@ -18,6 +22,10 @@ export default function App() {
         screenOptions={{
           headerShown: false
         }}>
+          <Stack.Screen name="homePage" component={HomePage}></Stack.Screen>
+             <Stack.Screen name="libraryPage" component={LibraryPage}></Stack.Screen>
+             <Stack.Screen name="readingRoom" component={ReadingRoom}></Stack.Screen>
+             
             <Stack.Screen name="landingPage" component={LandingPage}></Stack.Screen>
             <Stack.Screen name="register" component={Register}></Stack.Screen>
             <Stack.Screen name="login" component={Login}></Stack.Screen>
