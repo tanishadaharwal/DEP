0f45b75c85f7e5beb701bd0941328c9331b3772c
SiddhiApraj7 <2021mcb1247@iitrpr.ac.in>
2024-03-02 01:25:08 +0530

    home page

---
+++ Frontend/src/component/Animation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export default function Animation() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <LottieView
+                autoPlay
+                loop
+                style={{ 
+                    width: 300, // Set width of the LottieView
+                    height: 300, // Set height of the LottieView
+                }}
+                source={require('../../assets/study.json')}
+            />
+        </View>
+    );
+}
