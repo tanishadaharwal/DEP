7d6e4adc36394343a77d140a86fe6e7d96f5ae34
SiddhiApraj7 <2021mcb1247@iitrpr.ac.in>
2024-03-03 19:09:12 +0530

    Minor change

--- Frontend/App.js
+++ Frontend/App.js
@@ -9,6 +9,9 @@ import ProfilePage from "./src/screens/ProfilePage";
 import QueryForm from "./src/screens/QueryForm";
 import Register from "./src/screens/Register";
 import Login from "./src/screens/Login";
+import HomePage from "./src/screens/HomePage";
+import LibraryPage from "./src/screens/LibraryPage";
+import ReadingRoom from "./src/screens/ReadingRoom";
 
 //Importing all screens
 export default function App() {
@@ -22,11 +25,16 @@ export default function App() {
           headerShown: false
         }}
       >
+        <Stack.Screen name="register" component={Register} />
+        <Stack.Screen name="queryForm" component={QueryForm} />
+        <Stack.Screen name="homePage" component={HomePage} />
         <Stack.Screen name="landingPage" component={LandingPage} />
         <Stack.Screen name="profilePage" component={ProfilePage} />
-        <Stack.Screen name="queryForm" component={QueryForm} />
-        <Stack.Screen name="register" component={Register} />
+        
+        
         <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="libraryPage" component={LibraryPage}></Stack.Screen>
+        <Stack.Screen name="readingRoom" component={ReadingRoom}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
 
