b144277ae4b2deeac466faae023c3d9348ba6600
Sakshi-196 <2021mcb1244@iitrpr.ac.in>
2024-03-03 18:50:14 +0530

    table color changed

--- Frontend/App.js
+++ Frontend/App.js
@@ -9,19 +9,18 @@ import ProfilePage from "./src/screens/ProfilePage";
 import QueryForm from "./src/screens/QueryForm";
 import Register from "./src/screens/Register";
 import Login from "./src/screens/Login";
-<<<<<<< HEAD
 import LostAndFound from "./src/screens/LostFound";
-
-=======
 import LibraryPage from "./src/screens/LibraryPage";
 import ReadingRoom from "./src/screens/ReadingRoom";
+import StudyRoom from "./src/screens/StudyRoom";
+import CommonRoom2 from "./src/screens/CommonRoom2";
 import Lb1 from "./src/screens/Lb1";
 import CommonRoom1 from "./src/screens/CommonRoom1";
 import DiscussionRoom from "./src/screens/DiscussionRoom";
+import HomePage from "./src/screens/HomePage";
 import ElectronicLab from "./src/screens/ElectronicLab";
-import StudyRoom from "./src/screens/StudyRoom";
-import CommonRoom2 from "./src/screens/CommonRoom2";
->>>>>>> 5e432d8 (library pages added)
+
+
 
 //Importing all screens
 export default function App() {
@@ -33,10 +32,12 @@ export default function App() {
           headerShown: false,
         }}
       >
-<<<<<<< HEAD
-        <Stack.Screen name="lostFoundForm" component={LostAndFound} />
-=======
         <Stack.Screen name="libraryPage" component={LibraryPage} />
+        <Stack.Screen name="queryForm" component={QueryForm} />
+        <Stack.Screen name="profilePage" component={ProfilePage} />
+        <Stack.Screen name="landingPage" component={LandingPage} />
+
+        <Stack.Screen name="lostFoundForm" component={LostAndFound} />
         <Stack.Screen name="readingRoom" component={ReadingRoom} />
         <Stack.Screen name="lb1" component={Lb1} />
         <Stack.Screen name="commonRoom1" component={CommonRoom1} />
@@ -45,10 +46,6 @@ export default function App() {
         <Stack.Screen name="studyRoom" component={StudyRoom} />
         <Stack.Screen name="commonRoom2" component={CommonRoom2} />
 
->>>>>>> 5e432d8 (library pages added)
-        <Stack.Screen name="landingPage" component={LandingPage} />
-        <Stack.Screen name="profilePage" component={ProfilePage} />
-        <Stack.Screen name="queryForm" component={QueryForm} />
         <Stack.Screen name="register" component={Register} />
         <Stack.Screen name="login" component={Login} />
       </Stack.Navigator>
