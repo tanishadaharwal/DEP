e1911a4531b4bb113c9aaf74a46f788b18b84220
tanisha <tanisha22daharwal@gmail.com>
2024-03-31 22:03:22 +0530

    updated schema

--- Backend/models/Client.js
+++ Backend/models/Client.js
@@ -6,9 +6,35 @@ const clientSchema = new mongoose.Schema({
   seatStatus : Boolean,
   seatOccupied : String,
   isAdmin: Boolean,
+  inLibrary : Boolean,
   
   
 });
+
+const LostandFoundSchema = new mongoose.Schema({
+
+  
+  
+});
+
+const QuerySchema = new mongoose.Schema({
+ 
+  
+  
+});
+
+const SeatsSchema = new mongoose.Schema({
+  taken : Boolean,
+  floor : Number,
+  room : String,
+  area : String,
+  seatNumber : Number,
+  occupantEmail : String
+    
+});
+
+
+
 
 // Create a Mongoose model
 const Client = mongoose.model("Client", clientSchema);
